14ef7bc907e53978109cb4e393184960f9e73f6e
Tanner Francis <Tanner.Francis11@gmail.com>
2021-06-11 09:59:47 -0500

    fixed broken image url

--- src/Components/About.js
+++ src/Components/About.js
@@ -29,7 +29,7 @@ function About(props) {
       <h2 className="view-header">About Nate:</h2>
       <article className="about-nate-bio">
             <div className="about-bio-image">
-                <img alt="Nathan Francis" src="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.6435-9/79103947_2645076688904747_4208698857770450944_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=vMPd9mGR1OUAX-iE8JZ&_nc_ht=scontent-dfw5-1.xx&oh=93aa1364d1ebc0293ba951f51740bd9f&oe=609B7822" />
+                <img alt="Nathan Francis" src="https://artistcontent.s3.us-east-2.amazonaws.com/nateprofile.jpg" />
             </div>
           
             <p>Nate Francis is a photographic and sculptural artist who works with issues of identity and
